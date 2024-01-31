8d0c33a9ad4c4dc2d911d508ddb584da9ae2f614
Shami_Mourya <shamimourya9852@gmail.com>
2024-01-31 17:37:47 +0530

    Scroll back to top icon

---
+++ src/components/ScrollBackToTop.tsx
@@ -0,0 +1,12 @@
+import { FaArrowUp } from "react-icons/fa";
+
+
+const ScrollBackToTop = () => {
+  return (
+    <div className="h-10 w-10 bg-red-600 text-white justify-center items-center rounded-3xl cursor-pointer">
+        <FaArrowUp />
+    </div>
+  )
+}
+
+export default ScrollBackToTop;
