6d3074d0ba3796b3ef9579a7de0139c3a968d501
Shami_Mourya <shamimourya9852@gmail.com>
2024-01-31 17:38:14 +0530

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+// import ScrollBackToTop from "@components/ScrollBackToTop";
+
+import Header from "@components/Header"
+
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world</h1>
+    <Header />
+      {/* <ScrollBackToTop /> */}
     </>
   )
 }
