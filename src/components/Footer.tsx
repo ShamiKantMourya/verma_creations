26100e50800785573add824162fb782ee2b794c1
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-02 17:23:34 +0530

    subscribe option

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="flex flex-col mobile:h-auto bg-gray-950">
-                <div className="mobile:ml-8">
+                <div className="mobile:ml-8 mt-3">
                     <img src={logo1} alt="logo" className="cursor-pointer moblie:w-8 mobile:h-8 mb-2" />
                     <p className="flex text-slate-50 mobile:text-xs py-1">
                         ©Copyrights 2024 Verma Creations
@@ -19,7 +19,7 @@ const Footer = () => {
                     </span>
                 </div>
                 <div className="mobile:ml-8">
-                    <h2 className="text-slate-50">USEFUL LINKS</h2>
+                    <h2 className="text-slate-50 my-4">USEFUL LINKS</h2>
                     <div>
                         <ul>
                             <p className="text-gray-500 hover:text-slate-200">Home</p>
@@ -31,7 +31,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="mobile:ml-8">
-                    <h2 className="text-slate-50">LEGAL</h2>
+                    <h2 className="text-slate-50 my-4">LEGAL</h2>
                     <div>
                         <ul>
                             <p className="text-gray-500 hover:text-slate-200">Terms and Conditions</p>
@@ -41,14 +41,16 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="mobile:ml-8">
-                    <h2 className="text-slate-50">SUBSCRIBE</h2>
-                    <div className="flex items-center justify-start">
-                        <MdOutlineMailOutline className="text-gray-300"/>
-                        <input type="text" placeholder="Email" />
-                        <GrSend className="text-pink-600"/>
+                    <h2 className="text-slate-50 mt-4 mb-2">SUBSCRIBE</h2>
+                    <div className="flex items-center h-6 border-slate-100 border-b-2 mr-3 justify-between">
+                        <div className="flex justify-center items-center w-full">
+                        <MdOutlineMailOutline className="text-gray-300 mr-2"/>
+                        <input type="text" placeholder="Email" className="my-5 border-none bg-transparent outline-none text-slate-100 w-full" />
+                        </div>
+                        <GrSend className="text-pink-500 flex mr-9"/>
                     </div>
                 </div>
-                <div className="flex text-slate-50 items-center mobile:ml-8">
+                <div className="flex text-slate-50 items-center mobile:ml-8 my-5">
                     <IoLogoInstagram className="hover:text-red-500"/>
                     <FaXTwitter className="mobile:mx-3"/>
                     <FaFacebookF className="hover:text-blue-700"/>
