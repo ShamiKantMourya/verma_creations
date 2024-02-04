d304724f12d402c37155ecd95876e59de7d6a4f3
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-04 22:43:56 +0530

    address added

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -2,7 +2,15 @@
 
 const Address = () => {
   return (
-    <div>Address</div>
+    <div>
+        <h1>
+            WANT TO WORK WITH US
+        </h1>
+        <h2>CONTACT US</h2>
+        <p>(+91) phone number</p>
+        <p>Email id</p>
+        <p>Block A, Vishwakarma Colony, Pul Pehaladpur, New Delhi, 110044</p>
+    </div>
   )
 }
 
