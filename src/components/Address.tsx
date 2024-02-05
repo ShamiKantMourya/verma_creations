038f566e4c27382b5d65d6481eb65fd77e9d09e9
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-05 18:30:20 +0530

    css added

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -2,14 +2,14 @@
 
 const Address = () => {
     return (
-        <div className="flex flex-col justify-center h-auto w-full">
-            <h1>
+        <div className="flex flex-col justify-center h-auto w-full px-12">
+            <h1 className="flex justify-center text-slate-100 text-3xl text-center flex-wrap">
                 WANT TO WORK WITH US
             </h1>
-            <h2>CONTACT US</h2>
-            <p>(+91) phone number</p>
-            <p>Email id</p>
-            <p>Block A, Vishwakarma Colony, Pul Pehaladpur, New Delhi, 110044</p>
+            <h2 className="text-lg text-slate-100 flex justify-center">CONTACT US</h2>
+            <p className="text-slate-100 flex justify-center">(+91) phone number</p>
+            <p className="text-slate-100 flex justify-center">Email id</p>
+            <p className="text-slate-100 flex justify-center text-center break-words">Block A, Vishwakarma Colony, Pul Pehaladpur, New Delhi, 110044</p>
         </div>
     )
 }
