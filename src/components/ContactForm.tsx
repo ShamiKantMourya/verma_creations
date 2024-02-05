5bd96d08c08c1b3f76b186783b3a2816cefee19b
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-05 19:20:48 +0530

    contact form

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,16 @@
+
+const ContactForm = () => {
+  return (
+    <div>
+       <div>
+       <h1>REACH OUT VIA THE FORM BELOW</h1>
+        <input type="text" placeholder="Full Name"/>
+        <input type="text" placeholder="Email"/>
+        <input type="number" placeholder="Phone Number"/>
+        <button>SUBMIT FORM</button>
+       </div>
+    </div>
+  )
+}
+
+export default ContactForm;
