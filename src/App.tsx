566fc99c1a21dccbc98b7cf21ce781300a9ec911
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-08 17:44:37 +0530

    contact from rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 // import ScrollBackToTop from "@components/ScrollBackToTop";
 
 import Address from "@components/Address"
+import ContactForm from "@components/ContactForm"
 import Footer from "@components/Footer"
 import Header from "@components/Header"
 
 function App() {
 
   return (
-    <div className="flex flex-col h-auto w-full bg-black">
-      {/* <Header /> */}
+    <div className="flex flex-col">
+      <Header />
       {/* <ScrollBackToTop /> */}
+      <ContactForm />
       <Address />
       <Footer />
     </div>
