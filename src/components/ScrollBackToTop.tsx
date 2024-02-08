d04a734c3065ccb2909061f41ac6b188e0514973
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-08 18:31:38 +0530

    scroll on top function

--- src/components/ScrollBackToTop.tsx
+++ src/components/ScrollBackToTop.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
 import { FaArrowUp } from "react-icons/fa";
 
 
 const ScrollBackToTop = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const showScrollbar = () => {
+    if (window.scrollY >= 200) {
+      setShowScroll(true);
+    } else {
+      setShowScroll(false)
+    }
+  }
+
+  function goToTop() {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+  }
+  window.addEventListener("scroll", showScrollbar);
   return (
-    <div className="h-10 w-10 bg-red-600 text-white justify-center items-center rounded-3xl cursor-pointer">
-        <FaArrowUp />
+    <div className="flex fixed bottom-8 right-8">
+      {
+        showScroll ? (
+          <div className="flex h-12 w-12 bg-red-600 text-white justify-center items-center rounded-3xl" onClick={goToTop}>
+            <FaArrowUp />
+          </div>
+        ) : null
+      }
     </div>
   )
 }
