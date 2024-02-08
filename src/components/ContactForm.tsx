0fa907dc4fb0be3e9e50e354806dc2aa19bed2cd
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-08 17:43:50 +0530

    contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,14 @@
 
 const ContactForm = () => {
   return (
-    <div>
-       <div>
-       <h1>REACH OUT VIA THE FORM BELOW</h1>
-        <input type="text" placeholder="Full Name"/>
-        <input type="text" placeholder="Email"/>
-        <input type="number" placeholder="Phone Number"/>
-        <button>SUBMIT FORM</button>
-       </div>
+    <div className="flex justify-center w-full h-auto mt-20 bg-gray-700">
+      <div className="flex flex-col justify-evenly px-8 bg-black my-10">
+        <p className="flex justify-center text-slate-100  font-bold text-xl mt-16">REACH OUT VIA THE FORM BELOW</p>
+        <input type="text" placeholder="Full Name" className="bg-transparent mb-4 mt-8 text-slate-100 outline-none border-b-2"/>
+        <input type="text" placeholder="Email" className="bg-transparent my-4 text-slate-100 outline-none border-b-2"/>
+        <input className="bg-transparent my-4 text-slate-100 outline-none border-b-2" type="number" placeholder="Phone Number" minLength={10} required/>
+        <button className="flex items-center justify-center border-slate-100 h-10 p-2 text-slate-100 my-5 border-2 w-32 mb-20">SUBMIT FORM</button>
+      </div>
     </div>
   )
 }
