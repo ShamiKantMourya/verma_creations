28ffae11236729c5fcd07a397d2b9d84caa449c8
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-09 20:31:04 +0530

    service component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Address from "@components/Address"
 import ContactForm from "@components/ContactForm"
 import Footer from "@components/Footer"
 import Header from "@components/Header"
-import ScrollBackToTop from "@components/ScrollBackToTop"
+import ScrollBackToTop from "@components/ScrollBackToTop";
+import Services from "@components/Services";
 
 const App: React.FC = () => {
 
@@ -12,6 +13,7 @@ const App: React.FC = () => {
     <div className="flex flex-col">
       <Header />
       <ScrollBackToTop />
+      <Services />
       <ContactForm />
       <Address />
       <Footer />
