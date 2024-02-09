ae7f0c473f0841e27364d7c77ed1d67ec5f1113a
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-09 21:01:48 +0530

    div for images

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,7 +2,34 @@ import React from 'react';
 
 const Services: React.FC = () => {
   return (
-    <div>Services</div>
+    <div className='flex flex-col flex-wrap mt-20 h-auto justify-between bg-black'>
+      <div>
+        <h2>What We Do</h2>
+      </div>
+      <div>
+        <h1>OUR SERVICES</h1>
+      </div>
+      <div>
+        <div>
+          <img src='' alt='Signage and Hoardings' />
+          <div>
+            <h1>Signage and Hoardings</h1>
+          </div>
+        </div>
+        <div>
+          <img src='' alt='Customize Wallpaper' />
+          <div>
+            <h1>Customize Wallpaper</h1>
+          </div>
+        </div>
+        <div>
+          <img src='' alt='Board Repair' />
+          <div>
+            <h1>Board Repair</h1>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
