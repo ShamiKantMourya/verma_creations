dbd9f5918891bbfbe1a7fa9c51a26fd04f0e4f49
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-11 20:56:44 +0530

    text-gradient

--- src/components/Address.tsx
+++ src/components/Address.tsx
@@ -3,7 +3,7 @@
 const Address = () => {
     return (
         <div className="flex flex-col justify-center h-auto w-full px-12 pb-12 bg-black">
-            <h1 className="flex justify-center text-slate-100 text-3xl text-center font-bold py-12">
+            <h1 className="flex justify-center text-slate-100 text-3xl text-center font-bold py-12 bg-gradient-to-r from-cyan-500 to-red-500 bg-clip-text text-transparent">
                 WANT TO WORK WITH US
             </h1>
             <h2 className="text-lg text-slate-100 flex justify-center font-semibold py-6">CONTACT US</h2>
