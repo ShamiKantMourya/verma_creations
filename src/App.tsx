39c13ad09368fe3a6ef1c58f3a81ba484db9bfe1
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-12 21:29:51 +0530

    Homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 
-import Address from "@components/Address"
-import ContactForm from "@components/ContactForm"
-import Footer from "@components/Footer"
-import Header from "@components/Header"
-import ScrollBackToTop from "@components/ScrollBackToTop";
-import Services from "@components/Services";
+import Homepage from "@components/Homepage/Homepage";
 
 const App: React.FC = () => {
 
   return (
     <div className="flex flex-col">
-      <Header />
-      <ScrollBackToTop />
-      <Services />
-      <ContactForm />
-      <Address />
-      <Footer />
+      <Homepage />
     </div>
   )
 }
