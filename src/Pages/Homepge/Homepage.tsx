ad348c32aa7465ab8e6b0e71a2d4ea8aac86a008
Shami_Mourya <shamimourya9852@gmail.com>
2024-02-14 19:01:01 +0530

    homepage

---
+++ src/Pages/Homepge/Homepage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Header from "@components/Header";
+import Footer from '@components/Footer';
+import Address from '@components/Address';
+import ContactForm from '@components/ContactForm';
+import ScrollBackToTop from '@components/ScrollBackToTop';
+import Services from '@components/Services';
+import Slider from '@components/Slider';
+
+const Homepage: React.FC = () => {
+    return (
+        <div>
+            <Header />
+            <ScrollBackToTop />
+            <Slider />
+            <Services />
+            <ContactForm />
+            <Address />
+            <Footer />
+        </div>
+    )
+}
+
+export default Homepage;
