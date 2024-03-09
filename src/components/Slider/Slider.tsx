202aa8b99a372cf400acabe5d19ff4e0b633d854
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-09 21:00:47 +0530

    slider component

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {sliderData} from "../../Utils/imageSlider";
+import Dots from './Dots';
+const Slider: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Slider;
