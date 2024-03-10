ce0899c6213bb33b00cc74c4accb1db01a28817d
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-10 19:24:38 +0530

    alt of image

--- src/Utils/imageSlider.ts
+++ src/Utils/imageSlider.ts
@@ -5,6 +5,7 @@ import Img4 from "../assets/pexels-san-engineer-1935370.jpg";
 
 export interface ImageSliderProps {
   id: Number | String;
+  alt: String;
   title: String;
   description: String;
   urls: String;
@@ -13,24 +14,28 @@ export interface ImageSliderProps {
 export const sliderData: ImageSliderProps[] = [
   {
     id: 1,
+    alt: 'Signage Board',
     title: "Signage Board Works",
     description: "Contact us for signage board works",
     urls: Img1,
   },
   {
     id: 2,
+    alt: 'Wallpaper',
     title: "Wallpaper Works",
     description: "Contact us for customised 2D and 3D wallpapers",
     urls: Img2,
   },
   {
     id: 3,
+    alt: "Repair Work",
     title: "Repair Works",
     description: "Contact us if you have repair works for your signage boards",
     urls: Img3,
   },
   {
     id: 4,
+    alt: "Neon lights",
     title: "Neon Works",
     description: "Contact us for making neon symbols and texts",
     urls: Img4,
