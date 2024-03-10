b5ba0f8342882be50ed989c855e69b09349f0ce1
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-10 19:10:19 +0530

    dots props

--- src/components/Slider/Dots.tsx
+++ src/components/Slider/Dots.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
 
-import {ImageSliderProps} from "../../Utils/imageSlider";
+import { ImageSliderProps } from "../../Utils/imageSlider";
 
 interface DotProps {
-    activeIndex: Number;
-    onClick: (id: Number | String) => void;
-    sliderData: ImageSliderProps[];
+  activeIndex: Number;
+  onClick: (id: Number | String) => void;
+  sliderData: ImageSliderProps[];
 }
 
 const Dots: React.FC<DotProps> = ({ activeIndex, onClick, sliderData }) => {
-    return (
-      <div>
-        {sliderData.map((data: ImageSliderProps) => {
-            console.log(data)
-          return (
-        
-            <div
-              key={Number(data.id)}
-              className={`${activeIndex === data.id ? "active-dot" : "dot"}`}
-              onClick={() => onClick(data.id)}
-            ></div>
-          );
-        })}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {sliderData.map((data: ImageSliderProps) => {
+        // console.log(data)
+        return (
+
+          <div
+            key={Number(data.id)}
+            className={`${activeIndex === data.id ? "dot active-dot" : "dot"}`}
+            onClick={() => onClick(data.id)}
+          ></div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Dots;
