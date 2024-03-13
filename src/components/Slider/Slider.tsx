e230b3b9f4f37e88642f5f115b5c37ed4d93b5cc
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-13 20:03:38 +0530

    removed slidercontent, arrow, dots components

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react';
 
-import SliderContent from './SliderContent';
 import { sliderData } from "../../Utils/imageSlider";
-import Arrows from './Arrows';
-import Dots from './Dots';
+import Carousel from "./Carousel";
 
-const len = sliderData.length - 1;
+
 const Slider: React.FC = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
     return (
         <div>
-            <SliderContent
-                activeIndex={activeIndex}
+            <Carousel
                 sliderData={sliderData}
-            />
-            <Arrows
-                prevSlide={() => setActiveIndex(activeIndex < 1 ? len : activeIndex - 1)}
-                nextSlide={() => setActiveIndex(activeIndex === len ? 0 : activeIndex + 1)}
-            />
-            <Dots
-                activeIndex={activeIndex}
-                sliderData={sliderData}
-                onClick={(activeIndex) => setActiveIndex(activeIndex)}
             />
         </div>
     )
