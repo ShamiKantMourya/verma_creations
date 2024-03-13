3d9c1d5b7d03f703a40864e74ebad716c41e349c
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-13 21:12:16 +0530

    change in id

--- src/Utils/imageSlider.ts
+++ src/Utils/imageSlider.ts
@@ -1,7 +1,7 @@
 import Img1 from "../assets/Signageboard.jpeg";
-import Img2 from "../assets/wallpaper.jpg";
-import Img3 from "../assets/repair1.jpg";
-import Img4 from "../assets/pexels-san-engineer-1935370.jpg";
+import Img2 from "../assets/pexels-san-engineer-1935370.jpg";
+import Img3 from "../assets/wallpaper.jpg";
+import Img4 from "../assets/repair1.jpg";
 
 export interface ImageSliderProps {
   id: number;
@@ -13,31 +13,31 @@ export interface ImageSliderProps {
 
 export const sliderData: ImageSliderProps[] = [
   {
-    id: 1,
+    id: 0,
     alt: 'Signage Board',
-    title: "Signage Board Works",
+    title: "Signage Works",
     description: "Contact us for signage board works",
     urls: Img1,
+  },
+  {
+    id: 1,
+    alt: "Neon lights",
+    title: "Neon Works",
+    description: "Contact us for making neon symbols and texts",
+    urls: Img2,
   },
   {
     id: 2,
     alt: 'Wallpaper',
     title: "Wallpaper Works",
     description: "Contact us for customised 2D and 3D wallpapers",
-    urls: Img2,
+    urls: Img3,
   },
   {
     id: 3,
     alt: "Repair Work",
     title: "Repair Works",
     description: "Contact us if you have repair works for your signage boards",
-    urls: Img3,
-  },
-  {
-    id: 4,
-    alt: "Neon lights",
-    title: "Neon Works",
-    description: "Contact us for making neon symbols and texts",
     urls: Img4,
   },
 ];
