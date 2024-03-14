e1ef958075563dd8412f706bf2f42cafbaf6d1b0
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-14 20:15:48 +0530

    prev and next function

--- src/components/Slider/Carousel.tsx
+++ src/components/Slider/Carousel.tsx
@@ -12,26 +12,24 @@ interface CarouselProps {
 const Carousel: React.FC<CarouselProps> = ({ sliderData, autoSlide, autoSlideInterval }) => {
     const [curr, setCurr] = useState(0);
 
-    const prev = () => {
-        setCurr((curr) => (curr === 0 ? sliderData.length - 1 : curr - 1))
-    };
-    const next = () => {
-        setCurr((curr) => (curr === sliderData.length - 1 ? 0 : curr + 1))
-    };
+    const prev = () => setCurr((curr) => (curr === 0 ? sliderData.length - 1 : curr - 1));
+    const next = () => setCurr((curr) => (curr === sliderData.length - 1 ? 0 : curr + 1));
+
 
     useEffect(() => {
         if (!autoSlide) return;
         const slideInterval = setInterval(next, autoSlideInterval)
         return () => clearInterval(slideInterval)
-    }, [])
+    }, []);
     return (
         <>
-            <div className='h-80 overflow-hidden relative'>
+            <div className='h-80 w-full overflow-hidden relative mt-20 lg:h-screen'>
                 {
                     sliderData.map((data) => (
-                        <div key={data.id} className='flex justify-center items-center transition-transform ease-out duration-500 z-0' style={{ transform: `translateX(-${curr * 100}%)` }}>
-                            <img src={data.urls} className='h-80 w-full' alt={data.alt} loading='lazy' />
-                            <div className='absolute px-10 z-10 opacity-95'>
+                        <div key={data.id} className='flex items-center transition-transform ease-out duration-500' style={{ transform: `translateX(-${curr * 100}%)` }}>
+                            <div className='bg-blue-950 h-80 w-full absolute top-0 opacity-50 lg:h-full'></div>
+                            <img src={data.urls} className='h-80 w-full lg:h-screen' alt={data.alt} />
+                            <div className='absolute px-10'>
                                 <h2 className='text-white text-4xl font-medium'>{data.title}</h2>
                                 <h3 className='text-white text-2xl py-2'>{data.description}</h3>
                             </div>
@@ -39,8 +37,7 @@ const Carousel: React.FC<CarouselProps> = ({ sliderData, autoSlide, autoSlideInt
                     ))
                 }
             </div>
-            <div className='bg-blue-950 h-80 w-full absolute top-0 mt-20 opacity-50'></div>
-            <div className='absolute inset-0 flex items-center justify-between p-2 h-80 mt-20'>
+            <div className='absolute inset-0 flex items-center justify-between p-2 h-80 mt-20 lg:h-full'>
                 <button onClick={prev} className='text-white'>
                     <ChevronLeft size={40} />
                 </button>
@@ -48,13 +45,13 @@ const Carousel: React.FC<CarouselProps> = ({ sliderData, autoSlide, autoSlideInt
                     <ChevronRight size={40} />
                 </button>
             </div>
-            <div className='absolute bottom-0 right-0 left-0 h-80'>
+            <div className='absolute bottom-0 right-0 left-0'>
                 <div className='flex items-center justify-center gap-2'>
-                {
-                    sliderData.map((data) => (
-                        <div key={data.id} className={`transition-all w-3 h-3 bg-white rounded-full ${curr === data.id ? "p-2" : "bg-opacity-50"}`}/>
-                    ))
-                }
+                    {
+                        sliderData.map((data) => (
+                            <div key={data.id} className={`transition-all w-3 h-3 bg-white rounded-full ${curr === data.id ? "p-2" : "bg-opacity-50"}`} />
+                        ))
+                    }
                 </div>
             </div>
         </>
