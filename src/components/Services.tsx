7f8162e4614bcb7a285ec30df13ac389c2eb8212
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-16 22:47:40 +0530

    change in image width

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -13,16 +13,16 @@ const Services: React.FC = () => {
       <div>
         <p className='bg-gradient-to-r from-cyan-500 to-red-500 bg-clip-text text-transparent flex justify-center font-bold text-4xl mb-5'>OUR SERVICES</p>
       </div>
-      <div className='flex flex-wrap justify-evenly px-8'>
-        <figure className='border-2 border-white rounded-md mt-3 w-auto h-96 cursor-pointer'>
+      <div className='flex flex-wrap justify-evenly px-8 w-full'>
+        <figure className='border-2 border-white rounded-md mt-3 h-96 cursor-pointer w-80'>
           <img src={Signage} alt='Signage and Hoardings' className='p-2 h-80' />
           <figcaption className='text-slate-100 flex justify-end text-xl font-medium pr-3 py-3'>Signage and Hoardings</figcaption>
         </figure>
-        <figure className='border-2 border-white rounded-md my-3 w-auto h-96 cursor-pointer'>
+        <figure className='border-2 border-white rounded-md my-3 w-80 h-96 cursor-pointer'>
           <img src={Wallpaper} alt='Customize Wallpaper' className='p-2 h-80' />
           <figcaption className='text-slate-100 flex justify-end text-xl font-immedium pr-3 py-3 '>Customize Wallpaper</figcaption>
         </figure>
-        <figure className='border-2 border-white rounded-md mb-3 w-auto h-96 cursor-pointer'>
+        <figure className='border-2 border-white rounded-md mobile:mb-3 w-80 h-96 cursor-pointer'>
           <img src={Repair} alt='Board Repair' className='p-2 h-80' />
           <figcaption className='text-slate-100 flex justify-end text-xl font-medium pr-3 py-3'>Board Repair</figcaption>
         </figure>
