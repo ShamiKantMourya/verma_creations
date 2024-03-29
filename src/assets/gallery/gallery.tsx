ff55fd2c56e838363473083363f6f44b9924db94
Shami_Mourya <shamimourya9852@gmail.com>
2024-03-29 07:51:27 +0530

    gallery images

---
+++ src/assets/gallery/gallery.tsx
@@ -0,0 +1,83 @@
+import Oryn from "./Oryn.jpeg";
+import Animo from "./Animo.jpeg";
+import Shot from "./21Shot.jpeg";
+import Wallpaper from "./3DWallpaper.jpeg";
+import H2R from "./H2Rmusic.jpeg";
+import Monk from "./Monk.jpeg";
+import Mrchef from "./MrChef.jpeg";
+import Nitra from "./NitaraJewels.jpeg";
+import Vanguard from "./Vanguard.jpeg";
+import Wallpaper2D from "./wallpaper.jpeg";
+import Swadeshi from "./swadeshi.jpeg";
+import FitFusion from "./FitFushion.jpeg";
+import DevOpticals from "./devOpticals.jpeg";
+import Shipping from "./shipping.jpeg";
+import CashForGold from "./CashForGold.jpeg";
+import BarbequeNation from "./BarbequeNation.jpeg";
+
+export const Images = [
+{
+    route: "/src/assets/gallery/Oryn.jpeg",
+    imgSrc: Oryn
+},
+{
+    route: "/src/assets/gallery/Animo.jpeg",
+    imgSrc: Animo
+},
+{
+    route: "/src/assets/gallery/21Shot.jpeg",
+    imgSrc: Shot
+},
+{
+    route: "/src/assets/gallery/BarbequeNation.jpeg",
+    imgSrc: BarbequeNation
+},
+{
+    route: "/src/assets/gallery/CashForGold.jpeg",
+    imgSrc: CashForGold
+},
+{
+    route: "/src/assets/gallery/shipping.jpeg",
+    imgSrc: Shipping
+},
+{
+    route: "/src/assets/gallery/devOpticals.jpeg",
+    imgSrc: DevOpticals
+},
+{
+    route: "/src/assets/gallery/FitFushion.jpeg",
+    imgSrc: FitFusion
+},
+{
+    route: "/src/assets/gallery/swadeshi.jpeg",
+    imgSrc: Swadeshi
+},
+{
+    route: "/src/assets/gallery/3DWallpaper.jpeg",
+    imgSrc: Wallpaper
+},
+{
+    route: "/src/assets/gallery/Vanguard.jpeg",
+    imgSrc: Vanguard
+},
+{
+    route: "/src/assets/gallery/wallpaper.jpeg",
+    imgSrc: Wallpaper2D
+},
+{
+    route: "/src/assets/gallery/NitaraJewels.jpeg",
+    imgSrc: Nitra
+},
+{
+    route: "/src/assets/gallery/MrChef.jpeg",
+    imgSrc: Mrchef
+},
+{
+    route: "/src/assets/gallery/Monk.jpeg",
+    imgSrc: Monk
+},
+{
+    route: "/src/assets/gallery/H2Rmusic.jpeg",
+    imgSrc: H2R
+},
+]
