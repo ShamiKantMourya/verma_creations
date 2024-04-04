e3c9a8b4034d9e810a96d6e17928e494ebd5bf52
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-04 15:18:15 +0530

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
-const Homepage = lazy(() => import("./Pages/Homepge/Homepage"));
+import Homepage from "./Pages/Homepge/Homepage";
 const Gallery = lazy(() => import("./Pages/Gallery/Gallery"));
 const Service = lazy(() => import("./Pages/Services/Service"));
-const About = lazy(() => (import("./Pages/About/About")));
+const About = lazy(() => import("./Pages/About/About"));
+const Error = lazy(() => import("./components/Error"));
+import Loader from "@components/Loader";
 
 const App: React.FC = () => {
 
@@ -13,18 +15,23 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/services" element={
-          <Suspense fallback={<div>Component is still loading..</div>}>
+          <Suspense fallback={<Loader />}>
             <Service />
           </Suspense>
         } />
         <Route path="/gallery" element={
-          <Suspense fallback={<div>Component is still loading..</div>}>
+          <Suspense fallback={<Loader />}>
             <Gallery />
           </Suspense>
         } />
         <Route path="/about" element={
-          <Suspense fallback={<div>Component is still loading..</div>}>
+          <Suspense fallback={<Loader />}>
             <About />
+          </Suspense>
+        } />
+        <Route path="*" element={
+          <Suspense fallback={<Loader />}>
+            <Error />
           </Suspense>
         } />
       </Routes>
