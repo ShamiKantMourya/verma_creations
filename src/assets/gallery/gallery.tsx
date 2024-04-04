1d35cad666d90ed10a733ed341a5b7b16e865f3f
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-04 13:05:26 +0530

    images added to public folder

--- src/assets/gallery/gallery.tsx
+++ src/assets/gallery/gallery.tsx
@@ -23,12 +23,12 @@ interface ImagesProps {
 
 export const Images: ImagesProps[] = [
     {
-        route: "/Oryn.jpeg",
+        route: "/src/assets/gallery/Oryn.jpeg",
         imgSrc: Oryn,
         alt: "Oryn"
     },
     {
-        route: "/Animo.jpeg",
+        route: "/src/assets/gallery/Animo.jpeg",
         imgSrc: Animo,
         alt: "Animo"
     },
