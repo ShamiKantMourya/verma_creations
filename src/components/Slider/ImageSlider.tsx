5ec4f28da7c468c26e5f37b21f4b9ba8bbe3cfb1
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-04 15:25:56 +0530

    change button into anchor tag

--- src/components/Slider/ImageSlider.tsx
+++ src/components/Slider/ImageSlider.tsx
@@ -24,10 +24,10 @@ const ImageSlider = () => {
                 <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
                     Signage Boards and 3D Wallpaper <br /> manufacturer
                 </motion.p>
-                <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-                    <a href="#contact">Contact now →</a>
+                <a href="#contact" className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+                    <span>Contact now →</span>
                     <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-                </button>
+                </a>
             </motion.div>
         </Carousel>
     );
