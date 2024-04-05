74cb83a1d1247b1e7cf3a46e19957fdc573af626
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-05 08:15:26 +0530

    add reason to contact

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -34,16 +34,20 @@ const ContactForm = () => {
             <Label htmlFor="phone Number" className="text-white">Phone Number</Label>
             <Input id="number" placeholder="+91-9754103802" type="number" />
           </LabelInputContainer>
+          <LabelInputContainer className="mb-4">
+            <Label htmlFor="text" className="text-white">Write your thought</Label>
+            <Input placeholder="Why you want to contact" type="text"></Input>
+          </LabelInputContainer>
 
           <button
-            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+            className="bg-gradient-to-br relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
           >
             Submit &rarr;
             <BottomGradient />
           </button>
 
-          <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+          <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent my-8 h-[1px] w-full" />
         </form>
       </div>
     </div>
