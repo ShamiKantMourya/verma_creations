8fe29b250dcd0abbfee37266e7ef0d6848430580
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-05 07:46:13 +0530

    lottie animation added

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+
+import LoaderAnimation from "../assets/animation/Loader.json";
 
 const Loader: React.FC = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: LoaderAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
   return (
-    <div>Loader</div>
+    <div className='flex justify-center items-start'>
+      <Lottie
+        options={defaultOptions}
+        height={200}
+        width={200}
+      />
+    </div>
   )
 }
 
