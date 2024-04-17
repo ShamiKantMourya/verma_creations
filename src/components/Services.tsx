f4494e52c729d58e64911300f097f440b38f7607
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-17 16:06:10 +0530

    tab responsive

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,12 +7,12 @@ const Services: React.FC = () => {
   return (
     <div className='flex flex-col justify-center h-auto w-full z-0 my-6'>
       <div>
-        <h2 className='text-slate-100 flex justify-center text-xl py-8'>What We Do</h2>
+        <h2 className='text-slate-100 flex justify-center mobile:text-xl py-8 lg:text-2xl tab:text-3xl'>What We Do</h2>
       </div>
       <div>
-        <p className='bg-gradient-to-r from-cyan-500 to-red-500 bg-clip-text text-transparent flex justify-center font-bold text-4xl mb-5'>OUR SERVICES</p>
+        <p className='bg-gradient-to-r from-cyan-500 to-red-500 bg-clip-text text-transparent flex justify-center font-bold text-4xl mb-5 tab:text-5xl'>OUR SERVICES</p>
       </div>
-      <div className="h-auto relative  flex items-center justify-evenly flex-wrap mobile:px-6">
+      <div className="h-auto relative flex flex-wrap mobile:px-6 md:px-6 lg:px-12 xl:justify-start lg:justify-center justify-center gap-12">
         {
           sliderData.map((data) => (
             <DirectionAwareHover imageUrl={data.urls} alt={data.alt} key={data.id}>
