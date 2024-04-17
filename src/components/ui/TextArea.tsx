8a4b128557c577a95990cdc783b2b0fedc6a2ec7
Shami_Mourya <shamimourya9852@gmail.com>
2024-04-17 16:07:14 +0530

    tab and ipad responsive

--- src/components/ui/TextArea.tsx
+++ src/components/ui/TextArea.tsx
@@ -47,7 +47,7 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
           focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-600
            disabled:cursor-not-allowed disabled:opacity-50
            dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-           group-hover/input:shadow-none transition duration-400
+           group-hover/input:shadow-none transition duration-400 tab:h-14 ipad:h-16 tab:text-2xl ipad:text-3xl
            `,
             className
           )}
